b28f09e35742a940243f794aa104ce12c2c89543
brito101 <rodrigobrito101@hotmail.com>
2022-02-07 14:43:12 -0300

    Finished config adminlte

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\SiteController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('admin', [AdminController::class, 'index'])->name('admin.home');
 });
+
+Route::get('/', [SiteController::class, 'index'])->name('site.home');
+
+Auth::routes();
